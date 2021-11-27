9c0ec9b67bd1c5df8e23fd9d07edd83615303fe2
mifeille <mniwemuhuza@bk.rw>
2021-11-28 00:08:08 +0200

    Updates

---
+++ precache-manifest.f5bdc7a58a5d6e0170bde6e84cfbea31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "175dc4a6fb45ec032b7a",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.ee8b8236.chunk.css"
+  },
+  {
+    "revision": "175dc4a6fb45ec032b7a",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.175dc4a6.chunk.js"
+  },
+  {
+    "revision": "de737069a793e2fbd6e2",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/1.5a5b8c31.chunk.css"
+  },
+  {
+    "revision": "de737069a793e2fbd6e2",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/1.de737069.chunk.js"
+  },
+  {
+    "revision": "2242dfc52fd5369ed931",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime~main.2242dfc5.js"
+  },
+  {
+    "revision": "b3bbb38cc56b27b9303cade0046093f9",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  }
+];
